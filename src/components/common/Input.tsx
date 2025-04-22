c050a15701b935f88535cabe129f35c429ad5863
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-04-22 21:44:37 +0900

    feat: 회원가입 페이지 인증번호 요청 시 수정 불가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
-import ErrorMessage from '../ErrorMessage';
-import { UseFormRegister, FieldValues, Path } from 'react-hook-form';
+import { ReactNode } from "react";
+import ErrorMessage from "../ErrorMessage";
+import { UseFormRegister, FieldValues, Path } from "react-hook-form";
 
 type InputProps<T extends FieldValues> = {
   label?: ReactNode;
@@ -8,23 +8,28 @@ type InputProps<T extends FieldValues> = {
   registerName: Path<T>;
   placeholder?: string;
   errorMessage?: string;
+  disabled?: boolean;
 };
 
 export default function Input<T extends FieldValues>({
-  label = '',
+  label = "",
   register,
   registerName,
   placeholder,
   errorMessage,
+  disabled = false,
 }: InputProps<T>) {
   return (
-    <div className='text-[15px] text-black text-opacity-70'>
+    <div className="text-[15px] text-black text-opacity-70">
       <label>{label}</label>
       <input
-        type='text'
+        type="text"
         {...register(registerName)}
         placeholder={placeholder}
-        className='border-[1.5px] border-solid border-[rgba(0,0,0,0.5)] rounded-[10px] w-full h-11 px-3'
+        className={`border-[1.5px] border-solid border-[rgba(0,0,0,0.5)] rounded-[10px] w-full h-11 px-3 ${
+          disabled && "bg-gray-200"
+        }`}
+        disabled={disabled}
       />
       <ErrorMessage message={errorMessage} />
     </div>
--- src/components/signUp/SignUpStepOne.tsx
+++ src/components/signUp/SignUpStepOne.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import LogoImg from '../../../public/signUpLogo.png';
-import { useAppDispatch } from '../../store/hooks';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { StepOneForm, stepOneSchema } from '../../schemas/stepOneSchema';
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import LogoImg from "../../../public/signUpLogo.png";
+import { useAppDispatch } from "../../store/hooks";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { StepOneForm, stepOneSchema } from "../../schemas/stepOneSchema";
 import {
   handleSendCodeLogic,
   handleVerifyCodeLogic,
-} from '../../utils/authHandlers';
-import Link from 'next/link';
-import Input from '../common/Input';
-import Button from '../common/Button';
+} from "../../utils/authHandlers";
+import Link from "next/link";
+import Input from "../common/Input";
+import Button from "../common/Button";
 
 export default function SignUpStepOne(): JSX.Element {
   const router = useRouter();
@@ -38,36 +38,38 @@ export default function SignUpStepOne(): JSX.Element {
   };
 
   return (
-    <div className='w-[393px] mx-auto flex flex-col justify-center h-[calc(100vh-73px)] font-[Pretendard]'>
-      <Link href='/landing'>
+    <div className="w-[393px] mx-auto flex flex-col justify-center h-[calc(100vh-73px)] font-[Pretendard]">
+      <Link href="/landing">
         <Image
           src={LogoImg}
-          alt='로고'
-          className='mx-auto w-[113px] h-[134px] mb-12'
+          alt="로고"
+          className="mx-auto w-[113px] h-[134px] mb-12"
           priority
         />
       </Link>
 
       <form
-        className='px-[56px] flex flex-col gap-4 mx-auto'
+        className="px-[56px] flex flex-col gap-4 mx-auto"
         onSubmit={handleSubmit(verificationVisible ? verifyCode : sendCode)}
       >
         {/* 이름 */}
         <Input
-          label='이름 입력'
+          label="이름 입력"
           register={register}
-          registerName='name'
-          placeholder='김한모'
+          registerName="name"
+          placeholder="김한모"
           errorMessage={errors.name?.message}
+          disabled={verificationVisible}
         />
 
         {/* 전화번호 */}
         <Input
-          label='휴대전화'
+          label="휴대전화"
           register={register}
-          registerName='phoneNumber'
-          placeholder='01012345678'
+          registerName="phoneNumber"
+          placeholder="01012345678"
           errorMessage={errors.phoneNumber?.message}
+          disabled={verificationVisible}
         />
 
         {/* <button
@@ -78,31 +80,31 @@ export default function SignUpStepOne(): JSX.Element {
           인증하기
         </button> */}
         <Button
-          name='인증하기'
+          name="인증하기"
           verificationVisible={verificationVisible}
-          className='mt-2'
+          className="mt-2"
         />
 
         {/* 인증 버튼 */}
-        <div className='flex flex-col gap-2'>
+        <div className="flex flex-col gap-2">
           {/* 인증번호 입력 및 확인 */}
           {verificationVisible && (
-            <div className='flex flex-col gap-6 mt-5'>
+            <div className="flex flex-col gap-6 mt-5">
               <Input
                 register={register}
-                registerName='authNumber'
-                placeholder='인증번호를 입력해주세요'
+                registerName="authNumber"
+                placeholder="인증번호를 입력해주세요"
                 errorMessage={errors.authNumber?.message}
               />
               {/* <button type='submit' className={buttonClass}>
                 인증확인
               </button> */}
-              <Button name='인증확인' />
+              <Button name="인증확인" />
             </div>
           )}
         </div>
       </form>
-      <Link href='/restore' className='text-center mt-20 underline'>
+      <Link href="/restore" className="text-center mt-20 underline">
         3일 이내 탈퇴한 회원은 여기를 눌러주세요.
       </Link>
     </div>
