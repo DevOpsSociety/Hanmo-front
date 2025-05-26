805b5619cf33c85975d33814179f468c2179af34
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-05-26 22:13:27 +0900

    style: SignUpStepTwo 페이지 구조 스타일링

--- src/components/signUp/SignUpStepTwo.tsx
+++ src/components/signUp/SignUpStepTwo.tsx
@@ -10,7 +10,6 @@ import { RootState } from "../../store";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { handleSignUpLogic } from "../../utils/authHandlers";
 import { enumToOptions, objectEnumToOptions } from "../../utils/enumToOptions";
-import Button from "../common/Button";
 import Option from "../common/Option";
 import InputField from "../InputField";
 
@@ -18,6 +17,7 @@ export default function SignUpStepTwo() {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const formData = useAppSelector((state: RootState) => state.signUp.formData);
+
   const [loading, setLoading] = useState(false);
 
   console.log();
@@ -44,7 +44,7 @@ export default function SignUpStepTwo() {
   return (
     <form
       onSubmit={handleSubmit(handleSignup)}
-      className="flex flex-col gap-2 w-full max-w-[393px] mx-auto justify-center mt-6 px-3 font-[manSeh]"
+      className="flex flex-col gap-2 w-full max-w-[393px] mx-auto justify-center pt-2 px-3 font-[manSeh] h-[calc(100vh-73px)]"
     >
       <InputField<StepTwoForm>
         name="studentNumber"
@@ -53,7 +53,7 @@ export default function SignUpStepTwo() {
         register={register}
       />
 
-      <div className="text-[#4AA0E8] text-[15px] text-left mb-2 ml-1">
+      <div className="text-[#4AA0E8] text-[13px] text-left mb-2 ml-1 font-[pretendard]">
         학번은 아이디로 사용되오니 정확히 입력해 주세요!
       </div>
 
@@ -87,18 +87,17 @@ export default function SignUpStepTwo() {
         errorMessage={errors.instagramId?.message}
         register={register}
       />
+      <div className="mt-auto pb-8 pt-16">
+        <button
+          type="submit"
+          className={`w-full h-[56px] rounded-xl bg-[#19446C] text-white text-[22px] font-[manseh] tracking-wider `}
+        >가입하고 별명 짓기
+        </button>
 
-      <Button
-        name={loading ? "가입 중..." : "가입하고 별명 짓기"}
-        className={
-          "mt-10 w-full h-14 bg-[#17406D] text-white text-[26px] font-[manSeh] rounded-xl"
-          + (loading ? " opacity-50 cursor-not-allowed" : "")
-        }
-      />
-
-      <p className="text-[#BDBDBD] text-[16px] text-center mt-4 font-[pretendard]">
-        입력하신 정보는 가입 일주일 후 자동 폐기됩니다
-      </p>
+        <div className="text-center text-xs text-gray-400 mt-3 font-[Pretendard]">
+          입력하신 정보는 가입 일주일 후 자동 폐기됩니다
+        </div>
+      </div>
     </form>
   );
 }
