98c63eed77520b9814f8c8c815ec74bdcbc013b5
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-06-04 02:55:34 +0900

    style: 채팅 페이지 퍼블리싱

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -21,14 +21,14 @@ const ChatPage = () => {
   }, []);
 
   return (
-    <div className="max-w-[393px] h-[calc(100dvh-90px)] mx-auto flex flex-col gap-4 font-[Pretendard] relative">
-      <div className="flex flex-col gap-6 px-6 overflow-y-auto max-h-[calc(100dvh-170px)] border-4 h-screen">
+    <main className="max-w-[1800px] h-[calc(100dvh-90px)] mx-auto flex flex-col gap-4 font-[Pretendard] relative">
+      <div className="flex flex-col gap-6 px-6 overflow-y-auto max-h-[calc(100dvh-170px)] h-screen">
         <ChatList chatHistory={chatHistory} nickname={nickname} />
       </div>
       <div className="flex flex-col w-full gap-3 absolute bottom-0">
         <ChatInput onSend={handleSendMessage} />
       </div>
-    </div>
+    </main>
   );
 };
 
